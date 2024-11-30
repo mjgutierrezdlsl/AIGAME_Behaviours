6962dcf9f0e1ce2327555554160bfe08ddbc68f9
Jolo Gutierrez <mariano.jose.gutierrez.iii@dlsl.edu.ph>
2024-12-01 00:41:58 +0800

    alert state now moves away from danger

--- Assets/_Projects/FiniteStateMachine/_Scripts/ResourceCollector/AlertState.cs
+++ Assets/_Projects/FiniteStateMachine/_Scripts/ResourceCollector/AlertState.cs
@@ -9,25 +9,48 @@ namespace AIGAME.FSM
         [field: SerializeField] public float Range { get; private set; } = 4f;
         [field: SerializeField] public LayerMask DangerLayer { get; private set; }
         [SerializeField] private float _coolDownTime = 1f;
+        [SerializeField] private float _moveSpeed = 1f;
 
         private CollectorStateController _controller;
         private float _elapsedTime;
 
         private Collider2D dangerCollider;
 
+        private Vector2 _moveDirection;
+        private bool _isFacingLeft;
+        
         protected override void OnStateEnter()
         {
             base.OnStateEnter();
             _controller = StateController as CollectorStateController;
             _controller.Collector.Animator.SetBool("isMoving", false);
-            _controller.Collector.Animator.SetTrigger("attack");
         }
 
         protected override void OnStateUpdate()
         {
             base.OnStateUpdate();
             dangerCollider = Physics2D.OverlapCircle(_controller.Collector.Position, Range, DangerLayer);
-            if (dangerCollider == null)
+            _controller.Collector.Animator.SetBool("isMoving", _moveDirection != Vector2.zero);
+            if (_moveDirection.x < 0)
+                _isFacingLeft = true;
+            else if (_moveDirection.x > 0) _isFacingLeft = false;
+
+            _controller.Collector.SpriteRenderer.flipX = _isFacingLeft;
+            if (dangerCollider != null)
+            {
+                if (_elapsedTime < _coolDownTime)
+                {
+                    _elapsedTime += Time.deltaTime;
+                    _moveDirection = _controller.Collector.Position - dangerCollider.transform.position;
+                    _moveDirection.Normalize();
+                }
+                else
+                {
+                    _elapsedTime = 0f;
+                    _moveDirection = Vector2.zero;
+                }
+            }
+            else
             {
                 if (_elapsedTime < _coolDownTime)
                 {
@@ -39,6 +62,13 @@ namespace AIGAME.FSM
                     _controller.ChangeState(_controller.Wander);
                 }
             }
+        }
+
+        protected override void OnPhysicsUpdate()
+        {
+            base.OnPhysicsUpdate();
+            _controller.Collector.Rigidbody.MovePosition((Vector2)_controller.Collector.Position +
+                                                        _moveDirection * (_moveSpeed * Time.fixedDeltaTime));
         }
     }
 }
